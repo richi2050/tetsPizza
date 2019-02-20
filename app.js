d505d8dc6e02370a26b572e7c15b086c6426a12d
Ricardo Lugo Recillas <ricardo.lugo.recillas@gmail.com>
2019-02-19 18:35:30 -0600

    paos Live

---
+++ app.js
@@ -0,0 +1,424 @@
+'use strict'
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const access_token = "EAAeEM8tABzsBAAnYRUdWCQeQLQP8eXAqeBuSWsgukCITx5K9TkqgpOD73vOqhnf3sTO72IjtJDTXTuidX0Y6Unq6N3r2VsvRxHec0XZCVzKyAYjN8UY19Xit4W5kMKI8BZAiPsQG42zEUoZC3s0J3EPrnIFRVkuyC8VTWlEBAZDZD";
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000 )); //Se configura el puerto
+app.use(bodyParser.json()); // asi se comunicaran y entenderemos los JSON que se comunicaran en nuestra App
+
+app.get('/',function(request, response){
+    response.send('Hola mundo ');
+});
+
+app.get('/webhook', function(request, response){
+    
+    if(request.query['hub.verify_token'] === 'test_pizza_token'){
+        //console.log(request.query);
+        response.send(request.query['hub.challenge']);
+    }else{
+        response.send('No tienes permisos necesarios a esta aplicación');
+    }
+});
+
+app.post('/webhook/', function(req, res){
+    const webhook_event = req.body.entry[0];
+    if(webhook_event.messaging) {
+        webhook_event.messaging.forEach(event => {
+            handleEvent(event.sender.id, event);
+        })
+    }
+    res.sendStatus(200);
+});
+
+function handleEvent(senderId, event){
+    if(event.message){
+        handleMessage(senderId, event.message)
+    }else if(event.postback){
+        handlePostback(senderId, event.postback.payload);
+    }
+}
+
+function handleMessage(senderId, event){
+    if(event.text){
+        contactSuppport(senderId);
+    }else if (event.attachments) {
+        handleAttachments(senderId, event)
+    }
+}
+
+function defaultMessage(senderId) {
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "message": {
+            "text": "Hola soy un bot de messenger y te invito a utilizar nuestro menu",
+            "quick_replies": [
+                {
+                    "content_type": "text",
+                    "title": "¿Quieres una Pizza?",
+                    "payload": "PIZZAS_PAYLOAD"
+                },
+                {
+                    "content_type": "text",
+                    "title": "Acerca de",
+                    "payload": "ABOUT_PAYLOAD"
+                }
+            ]
+        }
+    }
+    sendAction(senderId);
+    callSendApi(messageData);
+}
+
+function handlePostback(senderId, payload){
+    console.log(payload)
+    switch (payload) {
+        case "GET_STARTED_PUGPIZZA":
+            senderActions(senderId)
+            defaultMessage(senderId);
+        break;
+        case "PIZZAS_PAYLOAD":
+            senderActions(senderId)
+            showPizzas(senderId);
+        break;
+        case "PEPPERONI_PAYLOAD":
+            senderActions(senderId)
+            sizePizza(senderId);
+        break;
+        case "PERSONAL_SIZE_PAYLOAD":
+            senderActions(senderId)
+            getLocation(senderId);
+        break;
+        case "CONTACT_PAYLOAD":
+            senderActions(senderId);
+            contactSuppport(senderId);
+        break;
+        case "LOCATIONS_PAYLOAD":
+            senderActions(senderId);
+            showLocations(senderId);
+        break;
+        case "ABOUT_PAYLOAD":
+            senderActions(senderId);
+            messageImage(senderId);
+        break;
+        default:
+            defaultMessage(senderId);
+        break;
+    }
+}
+
+function sendAction(senderId){
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "sender_action": "typing_on"
+    };
+    callSendApi(messageData);
+}
+
+function handleAttachments(senderId, event){
+    let attachment_type = event.attachments[0].type;
+    switch (attachment_type) {
+        case "image":
+            console.log(attachment_type);
+        break;
+        case "video": 
+            console.log(attachment_type);
+        break;
+        case "audio":
+            console.log(attachment_type);
+        break;
+        case "file":
+            console.log(attachment_type);
+        break;
+        case "location":
+            receipt(senderId);
+        default:
+            console.log(attachment_type);
+        break;
+    }
+}
+
+function callSendApi(response) {
+    request({
+        "uri": "https://graph.facebook.com/me/messages",
+        "qs": {
+            "access_token": access_token
+        },
+        "method": "POST",
+        "json": response
+        },
+        function(err) {
+            if(err) {
+                console.log('Ha ocurrido un error')
+            } else {
+                console.log('Mensaje enviado')
+            }
+        }
+    )
+}
+
+function showPizzas(senderId) {
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [
+                        {
+                            "title": "Peperoni",
+                            "subtitle": "Con todo el sabor del peperoni",
+                            "image_url": "https://s3.amazonaws.com/chewiekie/img/productos-pizza-peperoni-champinones.jpg",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Elegir Pepperoni",
+                                    "payload": "PEPPERONI_PAYLOAD",
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Pollo BBQ",
+                            "subtitle": "Con todo el sabor del BBQ",
+                            "image_url": "https://s3.amazonaws.com/chewiekie/img/productos-pizza-peperoni-champinones.jpg",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Elegir Pollo BBQ",
+                                    "payload": "BBQ_PAYLOAD",
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    callSendApi(messageData)
+}
+
+function messageImage(senderId) {
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "message": {
+            "attachment": {
+                "type": "image",
+                "payload": {
+                    "url": "https://media.giphy.com/media/1dOIvm5ynwYolB2Xlh/giphy.gif"
+                }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
+function contactSuppport(senderId) {
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "text": "Hola este es el canal de soporte, ¿quieres llamarnos?",
+                    "buttons": [
+                        {
+                            "type": "phone_number",
+                            "title": "Llamar a un asesor",
+                            "payload": "+571231231231"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
+function showLocations(senderId) {
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "list",
+                    "top_element_style": "large",
+                    "elements": [
+                        {
+                            "title": "Sucursal Mexico",
+                            "image_url": "https://s3.amazonaws.com/chewiekie/img/productos-pizza-peperoni-champinones.jpg",
+                            "subtitle": "Direccion bonita #555",
+                            "buttons": [
+                                {
+                                    "title": "Ver en el mapa",
+                                    "type": "web_url",
+                                    "url": "https://goo.gl/maps/GCCpWmZep1t",
+                                    "webview_height_ratio": "full"
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Sucursal Colombia",
+                            "image_url": "https://s3.amazonaws.com/chewiekie/img/productos-pizza-peperoni-champinones.jpg",
+                            "subtitle": "Direccion muy lejana #333",
+                            "buttons": [
+                                {
+                                    "title": "Ver en el mapa",
+                                    "type": "web_url",
+                                    "url": "https://goo.gl/maps/GCCpWmZep1t",
+                                    "webview_height_ratio": "tall"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
+
+function sizePizza(senderId) {
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "message": {
+            attachment: {
+                "type": "template",
+                "payload": {
+                    "template_type": "list",
+                    "top_element_style": "large",
+                    "elements": [
+                        {
+                            "title": "Individual",
+                            "image_url": "https://s3.amazonaws.com/chewiekie/img/productos-pizza-peperoni-champinones.jpg",
+                            "subtitle": "Porcion individual de pizza",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Elegir Individual",
+                                    "payload": "PERSONAL_SIZE_PAYLOAD",
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Mediana",
+                            "image_url": "https://s3.amazonaws.com/chewiekie/img/productos-pizza-peperoni-champinones.jpg",
+                            "subtitle": "Porcion Mediana de pizza",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Elegir Mediana",
+                                    "payload": "MEDIUM_SIZE_PAYLOAD",
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
+
+function receipt(senderId) {
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "receipt",
+                    "recipient_name": "Oscar Barajas",
+                    "order_number": "123123",
+                    "currency": "MXN",
+                    "payment_method": "Efectivo",
+                    "order_url": "https://platzi.com/order/123",
+                    "timestamp": "123123123",
+                    "address": {
+                        "street_1": "Platzi HQ",
+                        "street_2": "---",
+                        "city": "Mexico",
+                        "postal_code": "543135",
+                        "state": "Mexico",
+                        "country": "Mexico"
+                    },
+                    "summary": {
+                        "subtotal": 12.00,
+                        "shipping_cost": 2.00,
+                        "total_tax": 1.00,
+                        "total_cost": 15.00
+                    },
+                    "adjustments": [
+                        {
+                            "name": "Descuento frecuent",
+                            "amount": 1.00
+                        }
+                    ],
+                    "elements": [
+                        {
+                            "title": "Pizza Pepperoni",
+                            "subtitle": "La mejor pizza de pepperoni",
+                            "quantity": 1,
+                            "price": 10,
+                            "currency": "MXN",
+                            "image_url": "https://s3.amazonaws.com/chewiekie/img/productos-pizza-peperoni-champinones.jpg"
+                        },
+                        {
+                            "title": "Bebida",
+                            "subtitle": "Jugo de Tamarindo",
+                            "quantity": 1,
+                            "price": 2,
+                            "currency": "MXN",
+                            "image_url": "https://s3.amazonaws.com/chewiekie/img/productos-pizza-peperoni-champinones.jpg"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
+function getLocation(senderId){
+    const messageData = {
+        "recipient": {
+            "id": senderId
+        },
+        "message": {
+            "text": "Ahora ¿Puedes proporcionarnos tu ubicación?",
+            "quick_replies": [
+                {
+                    "content_type": "location"
+                }
+            ]
+        }
+    }
+    callSendApi(messageData);
+}
+
+
+app.listen(app.get('port'),function(){
+    console.log(" servicio comunicando",app.get('port'));
+});
